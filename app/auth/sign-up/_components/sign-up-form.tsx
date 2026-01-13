cf3c0de728527c4aa4d52d3703f452f4c6094f5d
Quiwi27 <dev.rostyslav.liashenko@gmail.com>
2026-01-14 00:48:20 +0200

    feat: add sign in form

--- app/auth/sign-up/_components/sign-up-form.tsx
+++ app/auth/sign-up/_components/sign-up-form.tsx
@@ -1,13 +1,19 @@
 import { ControlledInput } from '@/app/_components/input';
-
+import Link from 'next/link';
 
 export function SignUpForm() {
   return (
-    <form className="flex flex-col gap-4">
-      <ControlledInput placeholder="Email" value="" />
-      <ControlledInput placeholder="Password" value="" />
-      <ControlledInput placeholder="Confirm Password" value="" />
-      <button className="btn">Sign Up</button>
+    <form className="flex flex-col gap-6">
+      <div className="flex flex-col gap-4">
+        <ControlledInput placeholder="Email" value="" />
+        <ControlledInput placeholder="Password" value="" />
+        <ControlledInput placeholder="Confirm Password" value="" />
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <button className="btn">Sign Up</button>
+        <p className="text-center">Already have an account? <Link href="sign-in/">Login here</Link></p>
+      </div>
     </form>
   );
 }
